9d9c098301ec03e6c65ad70afed6733f1d16409c
babsman123 <babspropertyconsult@gmail.com>
2023-11-16 14:12:08 +0100

    another commit

--- index.js
+++ index.js
@@ -13,36 +13,53 @@ btnSubmit.addEventListener("click", () => {
   const month = document.querySelector(".month").value;
   const year = document.querySelector(".year").value;
 
-  if (typeof (day && month && year) === String) {
-    // console.log("error");
-  } else {
-    let date = new Date();
+  let date = new Date();
 
-    let d2 = date.getDate();
-    let m2 = date.getMonth();
-    let y2 = date.getFullYear();
+  let d2 = date.getDate();
+  let m2 = 1 + date.getMonth();
+  let y2 = date.getFullYear();
+  let calMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-    let d = d2 - day;
-    let m = m2 - month;
-    let y = y2 - year;
-    console.log(d);
-    console.log(m);
-    console.log(y);
-    yearNum.textContent = y;
-    monthNum.textContent = m;
-    dayNum.textContent = d;
+  console.log(m2);
+  console.log(d2);
+  console.log(y2);
+
+  if (day > d2) {
+    d2 = d2 + calMonth[m2 - 1];
+    m2 = m2 - 1;
+  }
+  if (month > m2) {
+    m2 = m2 + 12;
+    y2 = y2 - 1;
   }
 
+  let d = d2 - day;
+  let m = m2 - month;
+  let y = y2 - year;
+  console.log(d);
+  console.log(m);
+  console.log(y);
+
+  yearNum.textContent = y;
+  monthNum.textContent = m;
+  dayNum.textContent = d;
   //   console.log(day);
 
   //   console.log(m2);
 });
 
-// console.log(date);
+// yearNum.textContent = y;
+// monthNum.textContent = m;
+// dayNum.textContent = d;
+// let d = d2 - day;
+//   let m = m2 - month;
+//   let y = y2 - year;
+//   console.log(d);
+//   console.log(m);
+//   console.log(y);
 
-// let d2 = date.getDate();
-// let m2 = 1 + date.getMonth();
-// let y2 = date.getFullYear();
-// // console.log(d2);
-// console.log(m2);
-// console.log(y2);
+// let mydate = new Date();
+// console.log(mydate);
+
+// let myMonth = 1 + mydate.getMonth();
+// console.log(myMonth);
